323d4470c073fd689d1ceadffbfb4a176986a8fa
adarsh <practiceadarsh@gmail.com>
2024-07-11 22:45:47 +0530

    Day -1 commit 2

---
+++ Chapter-1/DooBee.java
@@ -0,0 +1,16 @@
+class DooBee
+{
+    public static void main(String[] args)
+    {
+        int x = 0;
+        while (x < 2)
+        {
+            System.out.print("Doo");
+            System.out.print("Bee");
+            x++;
+        }
+        System.out.println("Do");
+    }
+}
+
+
