78d96fa7173f521068c625be283bdd9ec6adae89
adarsh <practiceadarsh@gmail.com>
2024-07-12 23:11:45 +0530

    add BottleSong

---
+++ Chapter-1/BottleSong.java
@@ -0,0 +1,29 @@
+class BottleSong
+{
+    public static void main(String[] args)
+    {
+        int BottleNum = 99;
+        String Name = "Bottles";
+        while (BottleNum > 0)
+        {
+            if (BottleNum == 1)
+            {
+                Name = "Bottle";
+            }
+            System.out.println(BottleNum + " Green " + Name + " Hanging on the wall");
+            System.out.println(BottleNum + " Green " + Name + " Hanging on the wall");
+            System.out.println("And if one Green Bottle should accidently fall, ");
+            BottleNum -= 1;
+
+            if (BottleNum > 0)
+            {
+                System.out.println("There will be " + BottleNum + " Green " + Name + " Hanging on the wall");
+            }
+            else
+            {
+                System.out.println("There will be no Green Bottles, hanging on the wall");
+            }
+        }
+    }
+}
+
